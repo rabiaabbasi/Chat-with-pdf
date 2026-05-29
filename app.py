3d1056ad9191db6d81e8dfc2c8e92e3ee403563f
Your Name <your@email.com>
2026-05-29 21:40:42 +0500

    Fix import error

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ warnings.filterwarnings('ignore')
 
 import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_openai import ChatOpenAI
